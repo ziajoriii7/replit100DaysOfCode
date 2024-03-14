ca9f4185f26a42d5b540039461476c1e97be63ee
ziajoriii <cybershadowprogress@gmail.com>
2024-03-14 00:00:46 -0500

    DAY 005 done: ADD day-005/main-day-005; if statements + else.py

---
+++ day-005/main-day-005; if statements + else.py
@@ -0,0 +1,24 @@
+print("== Who are you in the singularity? == ")
+print("dear computer wizard let's find out with the following quiz")
+print("please answer 'y' or 'n'")
+
+doomer = input("Know what's a P(doom)? ")
+if doomer == "y":
+    print("You must be Eliezer!")
+
+grimes = input("Do you that your heart will never feel will never know?")
+if grimes == "y":
+    print("""
+  And then it falls
+  And then I fall
+  And then I know
+  """)
+    print("...")
+    print("You must be Grimes")
+
+eacc = input("are you the acceleratoor?")
+if eacc == "y":
+    print("you are nietzche's reincarnation")
+else:
+    print("nooticer")
+
